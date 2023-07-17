a4b4c248a1a7112317dca99f9635f136ce12e92f
quassbottle <quassbottleyt@gmail.com>
2023-07-17 03:38:08 +0300

    Moved all IO logic to console.js

--- js/app.js
+++ js/app.js
@@ -1,68 +1,40 @@
-import { ConsoleCommand,DelayedOutputHandler } from "./console.js";
+import { ConsoleCommand,ConsoleHandler } from "./console.js";
 
 const consoleContainer = document.querySelector(".console");
 const consoleInput = consoleContainer.querySelector(".console__input");
 const consoleLog = consoleContainer.querySelector(".console__log");
 const consoleTypeInput = consoleInput.querySelector(".type-input");
-const consoleInputTextArea = consoleContainer.querySelector(".input-handler");
 
-const delayedOuputHandler = new DelayedOutputHandler();
+const console = new ConsoleHandler(consoleLog, consoleTypeInput);
 
 function main() { // todo: will refactor everything here later after I find out the best way to do everything. This all was made just for testing purposes
     const ping = new ConsoleCommand("ping", "returns Pong!", () => {
-        writeLine("Pong!");
+        console.writeLine("Pong!");
     });
     const echo = new ConsoleCommand("echo", "returns Pong!", (args) => {
-        args.length === 0 ? writeLine("Empty string has given.") : writeLine(args);
+        args.length === 0 ? console.writeLine("Empty string has given.") : console.writeLine(args);
     });
 
     window.addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
-            const input = submitLine();
+            const input = console.submit();
             const cmd = input.split(' ')[0];
-            if (cmd === "ping") {
-                ping.execute();
+            if (cmd === "cls") {
+                console.clear();
             } else if (cmd === "echo") {
                 echo.execute(input.substring(5, input.length));
+            } else if (cmd === "ping") {
+                ping.execute();
+            } else if (cmd === "output-speed") {
+                console.changeFastOutput(!console.fastOutput);
+                console.writeLine(`Changed fast output to "${console.fastOutput}".`);
             }
             else if (input.length !== 0) {
-                writeLine(`Unknown executable nor command is "${input}".`)
+                console.writeLine(`Unknown executable nor command is "${input}".`)
             }
             e.preventDefault();
         }
     })
 }
-
-
-function submitLine() {
-/*    let newLine = document.createElement("p");
-    newLine.innerHTML = consoleInput.innerHTML;
-    //newLine.removeChild(newLine.querySelector(".cursor"));
-    consoleLog.append(newLine);
-    //consoleInputTextArea.value = "";
-    consoleTypeInput.value = "";
-    return newLine.querySelector(".type-input").value;*/
-    delayedOuputHandler.stop();
-    let newLine = document.createElement("p");
-    newLine.innerHTML = `<span class=\"username\">test@vpive.ru</span>:~$ `;
-    newLine.innerHTML += consoleTypeInput.innerHTML;
-    consoleLog.append(newLine);
-    let userInput = consoleTypeInput.innerHTML;
-    consoleTypeInput.innerHTML = "";
-    return userInput;
-}
-
-function writeLineFast(text) {
-    let newLine = document.createElement("p");
-    newLine.innerHTML = text;
-    consoleLog.append(newLine);
-}
-
-function writeLine(text) {
-    let newLine = document.createElement("p");
-    consoleLog.append(newLine);
-    delayedOuputHandler.begin(newLine, text);
-}
-
 
 main();
--- js/console.js
+++ js/console.js
@@ -1,27 +1,107 @@
-class ConsoleHandler {
-    delayedOutputHandler;
-    constructor(consoleContainer) {
+export class ConsoleHandler {
+    fastOutput;
+    #consoleLog;
+    #consoleInput;
+    #consoleOutputHandler;
+    #userInputLog;
 
+    constructor(consoleLog, consoleInput) {
+        this.#consoleOutputHandler = new ConsoleOutputHandler(consoleLog);
+        this.#consoleInput = consoleInput;
+        this.#consoleLog = consoleLog;
+        this.#userInputLog = [];
+
+        this.changeFastOutput(false);
     }
 
-    writeLine(string) {
-
-    }
-
-    writeLineDelayed(string) {
-
+    writeLine(text) {
+        this.#consoleOutputHandler.writeLine(text);
     }
 
     clear() {
-
+        this.#consoleOutputHandler.clear();
     }
 
-    handleInput() {
+    submit() {
+        this.#consoleOutputHandler.stopDelayedOutput();
 
+        let newLine = document.createElement("p");
+        newLine.innerHTML = `<span class=\"username\">test@vpive.ru</span>:~$ `;
+        newLine.innerHTML += this.#consoleInput.innerHTML;
+        this.#consoleLog.append(newLine);
+
+        let userInput = this.#consoleInput.innerHTML;
+        this.#consoleInput.innerHTML = "";
+
+        this.#userInputLog.push(userInput);
+
+        return userInput;
+    }
+
+    changeFastOutput(value) {
+        this.#consoleOutputHandler.fastOutput = value;
+        this.fastOutput = value;
+    }
+
+    set fastOutput(value) {
+        console.log(value);
+        this.#consoleOutputHandler.fastOutput = value;
+        this.fastOutput = value;
+    }
+
+    get fastOutput() {
+        return this.fastOutput;
+    }
+
+    get output() {
+        return this.#consoleOutputHandler;
     }
 }
 
-export class DelayedOutputHandler {
+class ConsoleInputHandler {
+    consoleInput;
+    constructor(consoleInput) {
+        this.consoleInput = consoleInput;
+    }
+
+}
+
+class ConsoleOutputHandler {
+    consoleLog;
+    delayedOutputHandler;
+    fastOutput;
+    constructor(consoleLog) {
+        this.delayedOutputHandler = new DelayedOutputHandler();
+        this.fastOutput = false;
+        this.consoleLog = consoleLog;
+    }
+
+    writeLine(string) {
+        this.fastOutput ? this.writeLineFast(string) : this.writeLineDelayed(string);
+    }
+
+    writeLineDelayed(text, delay = 25) {
+        let newLine = document.createElement("p");
+        this.consoleLog.append(newLine);
+        this.delayedOutputHandler.begin(newLine, text, delay);
+    }
+
+    writeLineFast(text) {
+        let newLine = document.createElement("p");
+        newLine.innerHTML = text;
+        this.consoleLog.append(newLine);
+    }
+
+    stopDelayedOutput() {
+        this.delayedOutputHandler.stop();
+    }
+
+    clear() {
+        this.consoleLog.innerHTML = "";
+    }
+}
+
+class DelayedOutputHandler {
     interval;
 
     bufferedText;
@@ -48,6 +128,10 @@ export class DelayedOutputHandler {
         this.bufferedElement.textContent = this.bufferedText;
         clearInterval(this.interval);
     }
+}
+
+class CommandHandler {
+
 }
 
 export class ConsoleCommand {
